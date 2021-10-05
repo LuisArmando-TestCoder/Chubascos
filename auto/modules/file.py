5afc6239d36829b2f0cf366d48543cd09babb779
l.murillo.baltodano <l.murillo.baltodano@accenture.com>
2021-10-05 11:45:14 -0600

    enhance rap

--- auto/modules/file.py
+++ auto/modules/file.py
@@ -1,10 +1,14 @@
 import os
 
-def executeConditionalPath(path, callback):
+def executeConditionalPath(path, callback, pathExistsCallback = lambda x: x):
     if not os.path.exists(path):
         print("Creating: ", path)
 
         callback(path)
+
+        return
+
+    pathExistsCallback(path)
 
 def createFile(path, contents):
     file = open(path, "w", encoding="utf-8")
